6e5105f4491ee196145d7e184af968e61015d7b6
unknown <raphael.soaresit@gmail.com>
2022-06-06 11:16:03 -0300

    changin port number to 3333 on server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3300, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('Server is running...')
 })
